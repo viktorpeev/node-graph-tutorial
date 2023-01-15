7da78770cd18e0c7101edbec81c12e8770fbfba1
Viktor <lulzsec123@yahoo.com>
2023-01-15 18:36:29 +0200

    Add CRUD mutations

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -28,6 +28,31 @@ const resolvers = {
                 movie.name='Batman'
             );
         }
+    },
+    Mutation: {
+        createUser:(parent, args)=>{
+            const user = args.input;
+            const lastId = UserList[UserList.length-1].id;
+            user.id = lastId + 1;
+            UserList.push(user);
+            return user;
+        },
+        updateUsername:(parent,args)=>{
+            const {id,newUsername} = args.input;
+            let userUpdated;
+            UserList.forEach((user)=>{
+                if(user.id === Number(id)){
+                    user.username = newUsername;
+                    userUpdated = user;
+                }
+            });
+            return userUpdated;
+        },
+        deleteUser:(parent,args)=>{
+            const id = args.id;
+            _.remove(UserList,(user)=>user.id===Number(id));
+            return null;
+        }
     }
 }
 
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -28,6 +28,23 @@ const typeDefs = gql`
         NETHERLANDS
     }
 
+    input CreateUserInput{
+        name: String!
+        username:String!
+        age:Int!
+        nationality:Nationalities = BRAZIL
+    }
+    input UpdateUsernameInput{
+        id:ID!
+        newUsername:String!
+    }
+
+    type Mutation {
+        createUser(input:CreateUserInput!):User
+        updateUsername(input: UpdateUsernameInput!):User
+        deleteUser(id:ID!):User
+    }
+
     type Query{
         users:[User!]!
         user(id:ID!):User!
