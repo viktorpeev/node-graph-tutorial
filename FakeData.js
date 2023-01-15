4135d1998eb6c55ba89b18ece8f43dbef8904093
Viktor <lulzsec123@yahoo.com>
2023-01-15 12:56:45 +0200

    Initial commit

---
+++ FakeData.js
@@ -0,0 +1,39 @@
+const UserList = [
+    {
+        id: 1,
+        name: 'John',
+        username: 'joseph',
+        age: 20,
+        nationality: 'Canada',
+    },
+    {
+        id: 2,
+        name: 'Pepe', 
+        username: 'pepethefrog',
+        age: 20,
+        nationality: 'Brazil',
+    },
+    {
+        id: 3,
+        name: 'Mata',
+        username: 'matahari',
+        age: 20,
+        nationality: 'Netherlands',
+    },
+    {
+        id: 4,
+        name: 'Monkey',
+        username: 'monkeypox',
+        age: 50,
+        nationality: 'Afrika',
+    },
+    {
+        id: 5,
+        name: 'Molly',
+        username: 'mollybloom',
+        age: 37,
+        nationality: 'United States',
+    },
+];
+
+module.exports = {UserList};
